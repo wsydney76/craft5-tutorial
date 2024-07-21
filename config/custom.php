e7c375fa1c51ac36cd83753abb83877052fecebe
wsydn <wsydney@web.de>
2024-07-21 19:51:28 +0200

    Matrix blocks improvements

--- config/custom.php
+++ config/custom.php
@@ -3,4 +3,5 @@
 return [
     'cardTransform' => ['width' => 400, 'height' => 200, 'format' => 'webp'],
     'cardsPerPage' => 6,
+    'fullWidthImageTransform' => ['width' => 2000, 'height' => 300, 'format' => 'webp'],
 ];
